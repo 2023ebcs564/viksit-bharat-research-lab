25c07c7e29f36370cf623449f1295220c2e42625
SAURABH ALARIA <2023ebcs564@online.bits-pilani.ac.in>
2026-07-22 14:31:26 +0530

    Temporarily disable production security

--- config/settings.py
+++ config/settings.py
@@ -169,11 +169,4 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # --------------------------------------------------
 
 if not DEBUG:
-    SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
-
-    SECURE_SSL_REDIRECT = False
-
-    SESSION_COOKIE_SECURE = True
-    CSRF_COOKIE_SECURE = True
-
-    SECURE_CONTENT_TYPE_NOSNIFF = True
+    pass
