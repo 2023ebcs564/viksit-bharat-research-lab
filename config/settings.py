3a62296a313d35d46a80af715374aa40c2b3094c
SAURABH ALARIA <2023ebcs564@online.bits-pilani.ac.in>
2026-07-23 21:27:49 +0530

    make footer social links dynamic via SiteSettings

--- config/settings.py
+++ config/settings.py
@@ -104,11 +104,12 @@ TEMPLATES = [
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
-        "OPTIONS": {
+       "OPTIONS": {
             "context_processors": [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "core.context_processors.site_settings",
             ],
         },
     },
